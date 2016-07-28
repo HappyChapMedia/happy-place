465ef57bde3fa249b40db986c349aa735278c718
Casey <info@caseyburton.com>
2016-07-28 00:03:40 -0700

    front in entry content

--- page-templates/front.php
+++ page-templates/front.php
@@ -17,27 +17,9 @@ get_header(); ?>
 
 <?php do_action( 'foundationpress_before_content' ); ?>
 <?php while ( have_posts() ) : the_post(); ?>
-<?php the_content(); ?>
-<div class="entry-content">
-	<section class="intro" role="main">
-		<div class="section-contain">
-			<p>Catering to the needs of liberated creative thinkers, Happy Place is an inspirational shared work environment located in the heart of <br><strong>Bellingham, WA</strong>.</p>
-		</div>
-	</section>
-	<section class="why">
-		<div class="section-contain">
-			<h2>Why</h2>
-			<p>Because we believe that the workplace should be exactly where you want to go in the morning.</p>
-		</div>
-	</section>
-	<section>
-		<div class="section-contain">
-			<h2>Our Mission</h2>
-			<p>Our mission is to support good ideas and good people by providing the perfect context for their success: a work environment which shuns unnecessary formality while encouraging collaboration and stress-free productivity.</p>
-		</div>
-	</section>
-	
-</div>
+	<div class="entry-content">
+		<?php the_content(); ?>
+	</div>
 
 <?php endwhile;?>
 <?php do_action( 'foundationpress_after_content' ); ?>
